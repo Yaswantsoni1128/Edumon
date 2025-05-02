af307b36d2fa568cab9dbbe8d060cb23c0924a05
Yaswantsoni1128 <yaswantsoni2005@gmail.com>
2025-05-03 00:45:33 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ const app = express();
 
 // Middleware
 // import cors from 'cors';
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: `${process.env.FRONTEND_URL}`, credentials: true }));
 
 app.use(express.json());
 
